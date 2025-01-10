490f5955a6bd49d160cb9bdfb59df15548ed772f
E-I-M-C <mormons.at@gmail.com>
2025-01-10 12:56:28 -0800

    Fixed Cart not displaying items and added functionallity to add multiple items

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,12 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const products = getLocalStorage("so-cart") || [];
+  products.push(product);
+  setLocalStorage("so-cart", products);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
