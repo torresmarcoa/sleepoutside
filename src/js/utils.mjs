abd028701da5bbca61b181ce68d8cfbe07e751cc
E-I-M-C <mormons.at@gmail.com>
2025-01-10 20:35:42 -0800

    Added team.txt

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -7,7 +7,8 @@ export function qs(selector, parent = document) {
 
 // retrieve data from localstorage
 export function getLocalStorage(key) {
-  return JSON.parse(localStorage.getItem(key));
+  const cart = localStorage.getItem(key);
+  return cart ? JSON.parse(cart) : [];
 }
 // save data to local storage
 export function setLocalStorage(key, data) {
