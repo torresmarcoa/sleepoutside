aacda5e258688975397fb058f280b0733ffc0b00
E-I-M-C <mormons.at@gmail.com>
2025-01-25 12:39:50 -0800

    Added cart quantity functionality

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -15,21 +15,24 @@ function cartItemTemplate(item) {
     <h2 class="card__name">${item.Name}</h2>
   </a>
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">Quantity: ${item.Quantity}</p>
+  <p class="cart-card__quantity">
+    <span class="cart-card__button minusButton" data-removeId="${item.Id}" data-change="minus">-</span>
+      Quantity: ${item.Quantity}
+    <span class="cart-card__button addButton" data-removeId="${item.Id}" data-change="add">+</span></p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
-  <span class="cart-card__removeButton" data-removeId="${item.Id}">X</span>
+  <span class="cart-card__button removeButton" data-removeId="${item.Id}">X</span>
 </li>`;
 };
 
 export default class ShoppingCart {
     constructor(key, parentSelector) {
-        this.key = key;
-        this.parentSelector = parentSelector;
-      }
+      this.key = key;
+      this.parentSelector = parentSelector;
+    }
 
     init () {
         this.renderCartContents();
-        this.addListenerRemoveButton();
+        this.addListenerButton();
     }
 
     renderCartContents() {
@@ -40,21 +43,48 @@ export default class ShoppingCart {
           renderCartLength();
         } else {
           renderListWithTemplate(cartItemTemplate, cartListHTML, cartItems);
-          this.addListenerRemoveButton();
+          this.addListenerButton();
         }
         this.renderTotalCart(cartItems);
       }
 
-    addListenerRemoveButton() {
-        const listElements = document.getElementsByClassName("cart-card");
-        for (const element of listElements) {
-          const removeButtonHTML = element.children[5];
-          removeButtonHTML.addEventListener("click", () => {
-            const productId = removeButtonHTML.dataset.removeid;
-            this.removeCartItem(productId);
+    addListenerButton() {
+      const listElements = document.getElementsByClassName("cart-card");
+      for (const element of listElements) {
+        const buttons = element.querySelectorAll("span");
+        for (const button of buttons) {
+          const productId = button.dataset.removeid;
+          const changeType = button.dataset.change || null;
+          const buttonType = button.classList;
+          button.addEventListener("click", (event) => {
+            event.stopImmediatePropagation();
+            if (buttonType[1] === "minusButton") {
+              this.changeCartItemQuantity(productId, changeType);
+            } else if (buttonType[1] === "addButton") {
+              this.changeCartItemQuantity(productId, changeType);
+            } else if (buttonType[1] === "removeButton") {
+              this.removeCartItem(productId);
+            }
           });
         }
       }
+    }
+
+    changeCartItemQuantity(id, operation) {
+      setTimeout(()=>{}, 1000);
+      const cartItems = getLocalStorage("so-cart");
+      const itemIndex = cartItems.findIndex((cartItem) => cartItem.Id === id);
+      if (operation === "minus" && cartItems[itemIndex].Quantity > 1) {
+        cartItems[itemIndex].Quantity -= 1;
+      } else if (operation === "add") {
+        cartItems[itemIndex].Quantity += 1;
+      }
+      cartItems.length === 0
+        ? localStorage.removeItem("so-cart")
+        : setLocalStorage("so-cart", cartItems);
+      this.renderCartContents();
+      renderCartLength();
+    }
 
     removeCartItem(id) {
         const cartItems = getLocalStorage("so-cart");
